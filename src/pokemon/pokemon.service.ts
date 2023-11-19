88d6ef97e99a01bde0984c9e410c9e725750bf56
Frank712 <franksuriel_7@hotmail.com>
2023-11-18 23:31:14 -0600

    ADD delete by id with validation

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -71,8 +71,13 @@ export class PokemonService {
   async remove(term: string) {
     // const pokemon = await this.findOne(term);
     // await pokemon.deleteOne();
-    const result = this.pokemonModel.findByIdAndDelete(term);
-    return result;
+    // const result = this.pokemonModel.findByIdAndDelete(term);
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: term });
+
+    if (deletedCount === 0) {
+      throw new BadRequestException(`Pokemon with id ${term} not found`);
+    }
+    return;
   }
 
   private handleException(error: any, operation?: string) {
