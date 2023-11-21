d0d275024d4a9c78814026173d41286cbf23cd34
Frank712 <franksuriel_7@hotmail.com>
2023-11-20 23:59:27 -0600

    ADD seed pokemons to database

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+
+  constructor(private pokemonService: PokemonService) {}
 
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach(({ name, url }) => {
+    const promises = data.results.map(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-      console.log({ name, no });
+      return this.pokemonService.create({ name, no });
     });
-    return data.results;
+    await Promise.all(promises);
+    return 'Seed executed successfully!';
   }
 }
