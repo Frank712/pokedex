49b491253bbc053b196d4d1d944c21816c3cdc01
Francisco Lino <franksuriel_7@hotmail.com>
2023-11-22 20:37:33 -0600

    FIX export and import pokemon module to seed service

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
-import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
-import { PokemonService } from 'src/pokemon/pokemon.service';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
-  constructor(private pokemonService: PokemonService) {}
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async executeSeed() {
+    await this.pokemonModel.deleteMany();
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
@@ -18,7 +23,7 @@ export class SeedService {
     const promises = data.results.map(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-      return this.pokemonService.create({ name, no });
+      return this.pokemonModel.create({ name, no });
     });
     await Promise.all(promises);
     return 'Seed executed successfully!';
